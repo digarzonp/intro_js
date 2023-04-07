157ea2a76fef88ee5122ba7a3b99712adfa35174
Daniela Irene Garzón Pinto <digarzonp@unal.edu.co>
2023-04-06 22:27:58 -0500

    version 1.0

---
+++ programa.js
@@ -0,0 +1,45 @@
+
+
+let a = 34;
+let b=56;
+console.log(a+b);
+
+//Aritmeticas: * + - / %
+//comparación:(condiciones
+// > < >= <= == === !=, los tres simbolos de igual tambien comparan el tipo de dato
+//siempre un valor booleano true flase
+
+console.log(a==b)
+
+// casting automatico :conversion automatica del tipo de dato
+
+//Operaciones logicas: AND OR 
+
+//AND: &&
+//OR: ||
+
+c=true
+d=false
+
+console.log(c && d)
+console.log(c || d)
+
+// los programas son secuencias: linea por linea
+//Romper la secuencialidad 
+//para evitar procesari ciertas lineas de codigo 
+
+//decisioń:
+//if(condición){}
+
+
+if (a<b){
+    console.log("a es menor que b");
+}
+
+// if(condicion){} else[}
+
+if(a>b){
+    console.log("a es mayor que b");
+}else{
+    console.log("a es menor que b");
+}
